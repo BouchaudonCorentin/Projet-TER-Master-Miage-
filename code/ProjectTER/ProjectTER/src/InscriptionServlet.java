8a693eee29745260058b686203eecb82c37fa0d4
BrianLenormand <brian.lenormand@a-018818.ups.u-psud.fr>
2018-02-05 12:33:16 +0100

    Connection and Inscription

---
+++ code/ProjectTER/ProjectTER/src/InscriptionServlet.java
@@ -0,0 +1,47 @@
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/InscriptionServlet")
+public class InscriptionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public InscriptionServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		Client client = (Client) request.getAttribute("client");
+		
+		try {
+		
+		DataBase dbi = (DataBase) request.getSession().getAttribute("dbi");
+				if (dbi == null) {
+				          dbi = new DataBase();
+				request.getSession().setAttribute("dbi", dbi);
+				}
+			request.setAttribute("client", dbi.connection(client));
+			RequestDispatcher rd = request.getRequestDispatcher("/Connection.jsp");
+			rd.forward(request, response);
+			
+			
+		} catch (Exception e) {
+			throw new ServletException (e);
+		
+		}
+	
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
+
